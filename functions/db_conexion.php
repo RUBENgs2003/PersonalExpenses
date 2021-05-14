0376a3521f63aa9f7c9e7ebd0f40bba3cae96164
Rubén García Sánchez <52624353+RUBENgs2003@users.noreply.github.com>
2021-05-14 18:47:59 +0200

    Update db_conexion.php

--- functions/db_conexion.php
+++ functions/db_conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli('localhost', 'root', 'Listoguapo1#', 'personalexpenses'); // 1-SERVER 2-USER 3-PASS 4-NAME
+$conn = new mysqli('localhost', 'root', 'YOUR PASSWORD', 'personalexpenses'); // 1-SERVER 2-USER 3-PASS 4-NAME
 
 if($conn->connect_error){
     echo $error -> $conn->$connect_error;
